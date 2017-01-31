87b2ba99b7d60bd5260bd539e192790efda062ba
Hemanth <hemanth.sagarb@outlook.com>
2017-01-31 09:51:34 +0530

    sql table create scripts without keys

---
+++ plinks-creation.sql
@@ -0,0 +1,33 @@
+-- MySQL dump 10.13  Distrib 5.5.47, for debian-linux-gnu (x86_64)
+--
+-- Host: 10.64.48.27    Database: enwiki
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.0.23-MariaDB-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pagelinks`
+--
+
+DROP TABLE IF EXISTS `pagelinks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pagelinks` (
+  `pl_from` int(8) unsigned NOT NULL DEFAULT '0',
+  `pl_namespace` int(11) NOT NULL DEFAULT '0',
+  `pl_title` varbinary(255) NOT NULL DEFAULT '',
+  `pl_from_namespace` int(11) NOT NULL DEFAULT '0') ENGINE=InnoDB DEFAULT CHARSET=binary;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pagelinks`
