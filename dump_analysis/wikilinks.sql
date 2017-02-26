0eca8711a826ffa38fe8d26643400e892f49a0ba
hemanthsagarb <hemanth.sagarb@outlook.com>
2017-02-26 11:46:06 +0530

    Create wikilinks.sql

---
+++ dump_analysis/wikilinks.sql
@@ -0,0 +1,15 @@
+create wikilinks 
+  ( norm varbinary(255), 
+    surface_form varbinary(255),
+    page_title varbinary(255),
+    type varchar(20),
+    count int(8)
+  );
+
+LOAD DATA INFILE 'wikilinks_analysis.csv' INTO TABLE surface_forms  FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
+
+create index idx_wikilinks_norm on wikilinks(norm);
+
+create index idx_wikilinks_sform on wikilinks(surface_form);
+
+
